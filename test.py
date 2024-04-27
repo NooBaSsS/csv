1073bac2381babf54849a906be306173f7c42765
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2024-04-28 02:07:28 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -46,7 +46,6 @@ def get_sort():
         right_row_idx = []
         for a in next_row:
             right_row_idx.append(base[a])
-        # print(right_row_idx)
         global sort_idx
         sort_idx = right_row_idx
 
@@ -56,7 +55,6 @@ def sort_data():
     сортирует каждый элемент raw_data в по порядку индексов,
     записанных в sort_idx
     '''
-
     for candidate in raw_data:
         temp_element = {}
         temp_dict = {}
@@ -67,9 +65,45 @@ def sort_data():
         sorted_data.append(temp_element)
 
 
+def qualify():
+    '''
+    отбрасывает не подходщих кандидатов
+    '''
+    '''
+    не менее 20 и не более 59 лет  key:3
+    150-190 см  key:4
+    зрение 1.0  key: 6
+    Master или PhD  key: 7
+    знание английского: true  key: 8
+    '''
+    for candidate in sorted_data:
+        if 20 <= int(candidate[3]) or int(candidate[3]) >= 59:
+            if 150 <= int(candidate[4]) or int(candidate[4]) >= 190:
+                if float(candidate[6]) == 1.0:
+                    if candidate[7] == 'Master' or candidate[7] == 'PhD':
+                        if candidate[8]:
+                            global approved
+                            approved.append(candidate)
+
+
+def sort_approved():
+    '''
+    сортрует допущенных в соответствии с правилами
+    приоритет от 27 до 37 лет
+    далее по алфавиту имя+фамилия
+    '''
+    for candidate in approved:  # TODO: работает неправильно, исправить
+        if int(candidate[3]) < 27 or int(candidate[3]) > 37:
+            approved.append(approved.pop(approved.index(candidate)))
+
+
 get_sort()
 get_raw_data()
 sort_data()
+qualify()
 
-print(sort_idx)
-print(sorted_data, 'сортированные')
+# print(sort_idx)
+# print(sorted_data, 'сортированные')
+print(approved, 'прошедшие')
+sort_approved()
+print(approved, 'прошедшие + сортировка')
